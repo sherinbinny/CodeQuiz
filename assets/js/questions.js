136248a562d75941c8a747451a7bf110395268f9
Sherin Binny <sherin.energyfalcon.com>
2024-04-01 20:45:45 +0100

    Added questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+
+
+var codingAssessment = [
+    {
+        "title": "Which HTML tag is used to define an internal style sheet?",
+        "choices": ["<script>", "<style>", "<css>"],
+        "answer": "<style>"
+    },
+    {
+        "title": "Which of the following is the correct syntax for referring to an external script called 'xxx.js'?",
+        "choices": ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>"],
+        "answer": "<script src='xxx.js'>"
+    },
+    {
+        "title": "How do you write 'Hello World' in an alert box in JavaScript?",
+        "choices": ["msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
+        "answer": "alert('Hello World');"
+    },
+    {
+        "title": "Which CSS property controls the text size?",
+        "choices": ["text-style", "font-size", "text-size"],
+        "answer": "font-size"
+    },
+    {
+        "title": "Which of the following is NOT a valid JavaScript variable name?",
+        "choices": ["2names", "_first_and_last_names", "FirstAndLast"],
+        "answer": "2names"
+    }      
+  ];
+  
