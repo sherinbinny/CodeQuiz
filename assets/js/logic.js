392288d4db2b85072b3c9362212c05566b911dea
Sherin Binny <sherin.energyfalcon.com>
2024-04-01 21:22:37 +0100

    Added comments in questions.js

---
+++ assets/js/logic.js
@@ -0,0 +1,3 @@
+
+document.getElementById('start').addEventListener('click', startQuiz);
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,30 +1,30 @@
 
-
+//Array of question objects
 var codingAssessment = [
     {
-        "title": "Which HTML tag is used to define an internal style sheet?",
-        "choices": ["<script>", "<style>", "<css>"],
-        "answer": "<style>"
+        "title": "Which HTML tag is used to define an internal style sheet?", // Question text
+        "choices": ["<script>", "<style>", "<css>"], // Array of choices
+        "answer": "<style>" // Correct answer
     },
     {
-        "title": "Which of the following is the correct syntax for referring to an external script called 'xxx.js'?",
-        "choices": ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>"],
-        "answer": "<script src='xxx.js'>"
+        "title": "Which of the following is the correct syntax for referring to an external script called 'xxx.js'?", // Question text
+        "choices": ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>"], // Array of choices
+        "answer": "<script src='xxx.js'>" // Correct answer
     },
     {
-        "title": "How do you write 'Hello World' in an alert box in JavaScript?",
-        "choices": ["msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
-        "answer": "alert('Hello World');"
+        "title": "How do you write 'Hello World' in an alert box in JavaScript?", // Question text
+        "choices": ["msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"], // Array of choices
+        "answer": "alert('Hello World');" // Correct answer
     },
     {
-        "title": "Which CSS property controls the text size?",
-        "choices": ["text-style", "font-size", "text-size"],
-        "answer": "font-size"
+        "title": "Which CSS property controls the text size?", // Question text
+        "choices": ["text-style", "font-size", "text-size"], // Array of choices
+        "answer": "font-size" // Correct answer
     },
     {
-        "title": "Which of the following is NOT a valid JavaScript variable name?",
-        "choices": ["2names", "_first_and_last_names", "FirstAndLast"],
-        "answer": "2names"
+        "title": "Which of the following is NOT a valid JavaScript variable name?", // Question text
+        "choices": ["2names", "_first_and_last_names", "FirstAndLast"], // Array of choices
+        "answer": "2names" // Correct answer
     }      
   ];
   
