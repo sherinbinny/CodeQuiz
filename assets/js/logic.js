53bbb858afcea259d2a6516a0ec669a7eba823e9
Sherin Binny <sherin.energyfalcon.com>
2024-04-01 21:30:12 +0100

    Function to begin quiz, hide start screen and begin timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,21 @@
 
-document.getElementById('start').addEventListener('click', startQuiz);
+// Call function beginQuiz when start button clicked
 
+document.getElementById('start').addEventListener('click', beginQuiz);
+
+
+let timer;
+let time = questions.length * 10;
+let currentQuestionNumber;
+
+function beginQuiz()
+{
+    // Hide start screen
+    document.getElementById('start-screen').classList.add('hide');
+    // Show questions screen
+    document.getElementById('questions').classList.remove('hide');
+    // Start the timer
+    timer = setInterval(timeIncrement, 1000);
+    // Show time
+    document.getElementById('time').textContent = time;
+}
