5800a0fe0497b76db482bf2e415713d70b533ad0
Sherin Binny <sherin.energyfalcon.com>
2024-04-01 21:43:12 +0100

    Function to retrieve the first question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,8 +6,8 @@ document.getElementById('start').addEventListener('click', beginQuiz);
 
 let timer;
 let time = questions.length * 10;
-let currentQuestionNumber;
 
+// Start Quiz
 function beginQuiz()
 {
     // Hide start screen
@@ -18,4 +18,22 @@ function beginQuiz()
     timer = setInterval(timeIncrement, 1000);
     // Show time
     document.getElementById('time').textContent = time;
+    // Get the first question
+    getQuestion();
 }
+
+
+let currentQuestionNumber = 0;
+
+// Retrieve and show the current question
+function getQuestion()
+{
+    const currentQuestion = questions[currentQuestionNumber];
+    // Show current question
+    document.getElementById('question-title').textContent = currentQuestion.title;
+    // Clear out the old question choices
+    const choicesEl = document.getElementById('choices');
+    choicesEl.innerHTML = '';
+
+    
+  }
